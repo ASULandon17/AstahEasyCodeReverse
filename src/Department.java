be4d228ac6caf808024f6bf9f4d5c98ab4b944f6
Landon <dmessmor@asu.edu>
2024-09-01 14:50:23 -0700

    Move java files to src folder for collective drag/drop

---
+++ src/Department.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Department {
+
+	public String departmentName;
+
+	public String departmentCode;
+
+	private ArrayList<Course> courses;
+
+	public void addCourse() {
+
+	}
+
+	public void removeCourse() {
+
+	}
+
+}
